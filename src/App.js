9276e6d3e1b40272c701518d24dc8a6329fdd36b
Manuel Chaparro <meny.chap@gmail.com>
2024-01-27 12:23:44 -0600

    actualizacion

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola Mundo con React
+          Actualizacion para GIT
         </p>
         <a
           className="App-link"
